adb73bae3c7d37162f0ddd22cd32d05779a00333
Chris R <funcktor@gmail.com>
2023-04-10 00:31:00 +0300

    v5.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
-import typescript from "rollup-plugin-typescript2";
-import { uglify } from "rollup-plugin-uglify";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import externals from "rollup-plugin-node-externals";
+import del from "rollup-plugin-delete";
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.tsx",
-  output: [
-    {
-      file: pkg.main,
-      format: "amd",
-      exports: "named",
-      sourcemap: false,
-      strict: false,
-    },
-  ],
-  plugins: [typescript(), uglify()],
-  external: ["react", "react-dom", "rxjs", "rxjs/operators"],
-};
+export default [
+  {
+    input: "./src/index.js",
+    plugins: [
+      del({ targets: "dist/*" }),
+      externals({ deps: true }),
+      nodeResolve({ extensions: [".js"] }),
+      babel({
+        babelHelpers: "runtime",
+        exclude: "**/node_modules/**",
+        extensions: [".js"],
+      }),
+    ],
+    output: [{ file: pkg.module, format: "esm", sourcemap: true, strict: true }],
+    external: ["react", "rxjs", "rxjs/operators"],
+  },
+];
