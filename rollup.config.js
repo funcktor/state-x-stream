97fc83e830bc66691aab6f24223e951232ef4a07
Chris R <funcktor@gmail.com>
2023-04-30 07:42:22 +0300

    v6.0.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,21 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import terser from "@rollup/plugin-terser";
-import packageJson from "./package.json" assert { type: "json" };
+const commonjs = require("@rollup/plugin-commonjs");
+const resolve = require("@rollup/plugin-node-resolve");
+const typescript = require("@rollup/plugin-typescript");
+const peerDepsExternal = require("rollup-plugin-peer-deps-external");
+const terser = require("@rollup/plugin-terser");
+const packageJson = require("./package.json");
 
 const plugins = [peerDepsExternal(), resolve(), commonjs(), terser()];
 const exclude = ["node_modules", "dist", "src/tests", "src/stories"];
 const tsConfig = { declaration: true, declarationDir: "./dist", rootDir: "src/", exclude };
 
-export default [
+module.exports = [
   // CommonJS
   {
     input: "src/index.ts",
     output: { dir: "./", exports: "default", entryFileNames: packageJson.main, format: "cjs" },
     plugins: [...plugins, typescript(tsConfig)],
   },
-
   // ES
   {
     input: "src/index.ts",
