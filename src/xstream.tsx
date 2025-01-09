40a6ebc4f6c9fd01ceb688f0318f6ef014a54c7a
Chris R <funcktor@gmail.com>
2025-01-09 21:32:32 +0200

    --

--- src/xstream.tsx
+++ src/xstream.tsx
@@ -1,42 +1,50 @@
-import React, { ComponentType, PropsWithRef } from "react";
+import React, { ComponentType } from "react";
 import StateObserver from "./StateObserver";
 import shallowEqual from "./shallowEqual";
 
-export function xstream(injector: any) {
-  const createObserved = <P extends object>(
-    Wrapped: ComponentType<PropsWithRef<any>>
-  ): ComponentType<P> => {
-    return class Component extends React.Component<P> {
-      observer: any;
+// Define the type for the injector function
+type InjectorFunction = (injProps: any) => any;
 
-      constructor(props: any) {
+// Define the xstream function
+export function xstream<P extends object>(injector: InjectorFunction) {
+  const createObserved = (Wrapped: React.Component<P>) => {
+    return class ObservedComponent extends React.Component {
+      private observer: StateObserver | null;
+
+      constructor(props: P) {
         super(props);
         this.observer = new StateObserver(injector, props);
-        this.state = this.observer.state$.value;
+        this.state = this.observer.state$.value || {}; // Ensure state is initialized
       }
 
       componentDidMount() {
         if (!this.observer) {
           this.observer = new StateObserver(injector, this.props);
         }
-        const next = (x: any) => this.setState(x);
-        const error = (x: any) => console.error(x);
+
+        const next = (state: Partial<P>) => this.setState(state);
+        const error = (err: unknown) => console.error("Observer Error:", err);
 
         this.observer.state$.subscribe({ next, error });
       }
 
-      shouldComponentUpdate(nextProps: any, nextState: any) {
-        this.observer.setProps(nextProps);
-        return !shallowEqual(this.state, nextState);
+      shouldComponentUpdate(nextProps: P, nextState: Partial<P>) {
+        if (this.observer) {
+          this.observer.setProps(nextProps);
+        }
+        return !shallowEqual(this.state, nextState) || !shallowEqual(this.props, nextProps);
       }
 
       componentWillUnmount() {
-        this.observer && this.observer.destroy();
-        this.observer = null;
+        if (this.observer) {
+          this.observer.destroy();
+          this.observer = null;
+        }
       }
 
       render() {
-        return <Wrapped {...this.state} />;
+        // Pass only props to the wrapped component
+        return <Wrapped {...this.props} {...(this.state as P)} />;
       }
     };
   };
